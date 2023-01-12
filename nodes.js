b93b101ce2889ce5e3b35d26b1358beb18afc72d
Martin Noring <martin.noring@gmail.com>
2023-01-12 21:28:12 +0100

    multiple axis

--- nodes.js
+++ nodes.js
@@ -106,6 +106,38 @@ let NewLogPostViewModel = function() {
                 {label: 'Relativ luftfuktighet',
                 data: humidity}
             ],
+            options: {
+                responsive: true,
+                interaction: {
+                  mode: 'index',
+                  intersect: false,
+                },
+                stacked: false,
+                plugins: {
+                  title: {
+                    display: true,
+                    text: 'Luften i badrummet'
+                  }
+                },
+                scales: {
+                  y: {
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                  },
+                  y1: {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+            
+                    // grid line settings
+                    grid: {
+                      drawOnChartArea: false, // only want the grid lines for one axis to show up
+                    },
+                  },
+                }
+            }
+            
         };
     }
 
